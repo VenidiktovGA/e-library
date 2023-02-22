df23479eae3e362b9e14b88b74f32878cfe2ceb9
GennadyVenidiktov <wenter3@outlook.com>
2023-02-22 13:56:40 +0500

    Электронная библиотека | Версия 2.2.0 add JUnit5 and maven wrapper
     - Подключил JUnit5
     - Подключил maven wrapper

---
+++ src/test/java/ru/venidiktov/service/BookServiceTest.java
@@ -0,0 +1,12 @@
+package ru.venidiktov.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BookServiceTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(false);
+    }
+}
