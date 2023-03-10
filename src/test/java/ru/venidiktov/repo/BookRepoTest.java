66a9d844079072f61937a833020f14dd7fe4ebf9
GennadyVenidiktov <wenter3@outlook.com>
2023-03-11 00:45:24 +0500

    Электронная библиотека | Версия 2.3.1 - Testing: TestContainers
     - Рабочий тес с testContainers

--- src/test/java/ru/venidiktov/repo/BookRepoTest.java
+++ src/test/java/ru/venidiktov/repo/BookRepoTest.java
@@ -1,52 +1,54 @@
 package ru.venidiktov.repo;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.GenericContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import ru.venidiktov.Application;
+import ru.venidiktov.model.Book;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@DataJpaTest
 @Testcontainers
+@ContextConfiguration(classes = {Application.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(value = {"/schema.sql", "/data.sql"})
 public class BookRepoTest {
 
+    @Autowired
+    private BookRepoJpa bookRepo;
+
+    @Autowired
+    private PersonRepoJpa personRepo;
+
     @Container
-    public static GenericContainer simpleWebServer
-            = new GenericContainer("alpine:3.2")
-            .withExposedPorts(80)
-            .withCommand("/bin/sh", "-c", "while true; do echo "
-                    + "\"HTTP/1.1 200 OK\n\nHello World!\" | nc -l -p 80; done");
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:13.3");
+//            .withDatabaseName("eLibrary")
+//            .withUsername("postgres")
+//            .withPassword("qwerty");
 
-    @Test
-    public void givenSimpleWebServerContainer_whenGetReuqest_thenReturnsResponse()
-            throws Exception {
-        String address = "http://"
-                + simpleWebServer.getContainerIpAddress()
-                + ":" + simpleWebServer.getMappedPort(80);
-        String response = simpleGetRequest(address);
-
-        assertEquals(response, "Hello World!");
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
     }
 
-    private String simpleGetRequest(String address) throws Exception {
-        URL url = new URL(address);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
+    @Test
+    public void isExistsBook() {
+        // TODO: 3/11/2023 Косяк так как подразумевается что имся будет в Upper 
+        List<Book> books = bookRepo.findBookLikeName("КОЛЕЦ");
+        assertEquals(1, books.size());
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-        in.close();
-
-        return content.toString();
     }
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,7 +1,10 @@
 --delete from users;
-delete from person;
+--delete from person;
 
---insert into Users (username, year_of_birth, password) values("user", 1980, "pass");
+insert into Users (username, year_of_birth, password) values('user', 1980, 'pass');
 
 insert into Person (surname, name, middle_name, birthday, gender)
 values ('Ivanov', 'Ivan', 'Ivanovitch', '1960-01-01', 'male');
+
+insert into Book (name, author, year_publishing)
+values ('Властелин колец', 'Джон Рональд Руэл Толкин', '1954-07-27');
