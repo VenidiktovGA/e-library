6177d05bf749fe2e218e819a8a6591ce112647d2
GennadyVenidiktov <wenter3@outlook.com>
2023-03-19 00:49:05 +0500

    Электронная библиотека | Версия 4.1.0 - SpringSecurity: Разлогинивание Logout
     - Реализовал выход пользователя из своей усетной записи

--- src/main/java/ru/venidiktov/config/SecurityConfig.java
+++ src/main/java/ru/venidiktov/config/SecurityConfig.java
@@ -24,7 +24,9 @@ public class SecurityConfig {
                 .formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/", true)
-                .failureUrl("/auth/login?error");
+                .failureUrl("/auth/login?error")
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login");
 
         return http.build();
     }
